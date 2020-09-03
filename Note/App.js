d09e441717ad4120a7a7ea0d0f2c8b59294c5fa7
t <38640424+poohcid@users.noreply.github.com>
2020-09-03 21:14:33 +0700

    fixed onChangeText

--- Note/App.js
+++ Note/App.js
@@ -1,39 +1,10 @@
-import { StatusBar } from "expo-status-bar";
+
 import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
 import Main from "./src/Main";
 
 export default function App() {
-  const [note, setNote] = useState([]);
-  const [text, setText] = useState("");
-  const noteHandle = (e) => {
-    setText(e.target.value);
-  };
-  const noteSubmit = (e) => {
-    if (text !== "") {
-      e.preventDefault();
-      setNote([...note, text]);
-      setText("");
-    }
-    console.log(note);
-  };
-  const noteDelete = (event, index) => {
-    const index1 = note.indexOf(index);
-    const arr1 = note.filter((value, index1) => {
-      if (index1 !== index) {
-        return value;
-      }
-    });
-    setNote(arr1);
-  };
   return (
-    <Main
-      noteSubmit={noteSubmit}
-      noteHandle={noteHandle}
-      text={text}
-      note={note}
-      noteDelete={noteDelete}
-    />
+    <Main/>
   );
   /*
   return (
--- Note/src/Main.js
+++ Note/src/Main.js
@@ -3,19 +3,26 @@ import React, { useState } from "react";
 import { StyleSheet, Text, View, TextInput, Button, Image } from "react-native";
 import Note from "./Note";
 
-const Main = ({ noteSubmit, noteHandle, text, note, noteDelete }) => {
-  /*
+const Main = () => {
   const [note, setNote] = useState([]);
   const [text, setText] = useState("");
   const noteHandle = (e) => {
-    setText(e.target.value);
+    setText(e);
   };
   const noteSubmit = (e) => {
     e.preventDefault();
     setNote([...note, text]);
     console.log(note);
   };
-  */
+  const noteDelete = (event, index) => {
+    const index1 = note.indexOf(index);
+    const arr1 = note.filter((value, index1) => {
+      if (index1 !== index) {
+        return value;
+      }
+    });
+    setNote(arr1);
+  };
   return (
     <View style={styles.container}>
       <Text>สมุดบันทึก</Text>
@@ -24,7 +31,7 @@ const Main = ({ noteSubmit, noteHandle, text, note, noteDelete }) => {
           style={styles.tinput}
           placeholder="บันทึกข้อความที่นี่"
           value={text}
-          onChange={noteHandle}
+          onChangeText={noteHandle}
         ></TextInput>
       </View>
       <View style={styles.input}>
@@ -33,10 +40,10 @@ const Main = ({ noteSubmit, noteHandle, text, note, noteDelete }) => {
       <View style={styles.input}>
         <Note arrText={note} noteDelete={noteDelete} />
       </View>
-      <View style={{ position: "absolute", bottom: 50, left: 200 }}>
+      <View style={{ position: "absolute", bottom: 20, right: 20 }}>
         <Image
           source={require("../assets/IT.png")}
-          style={{ width: 100, height: 100 }}
+          style={{ width: 80, height: 80 }}
         />
       </View>
     </View>
