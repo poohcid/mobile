23c8bccf8aab32d8f1633df0b0ad596c37d5e27c
LAB203\LAB203 <thanabordee@it.kmitl.ac.th>
2020-10-21 12:49:03 +0700

    fix

--- MealApp2/src/screens/FiltersScreen.js
+++ MealApp2/src/screens/FiltersScreen.js
@@ -1,13 +1,60 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
-
+import React, {useState} from "react";
+import { View, Text, StyleSheet, Switch, TouchableOpacity } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 const FiltersScreen = (props) => {
+  const [btn1, setBtn1] = useState(false)
+  const [btn2, setBtn2] = useState(false)
+  const [btn3, setBtn3] = useState(false)
+  const [btn4, setBtn4] = useState(false)
+
   return (
     <View style={styles.screen}>
-      <Text>The Filters Screen!</Text>
+      <View style={{flexDirection:'row'}}>
+        <Text>Gluten-free</Text>
+        <Switch trackColor={{true:'red', false:'blue'}} onValueChange={() => setBtn1(!btn1)} value={btn1}/>
+      </View>
+      <View style={{flexDirection:'row'}}>
+        <Text>Lactose-free</Text>
+        <Switch trackColor={{true:'red', false:'blue'}} onValueChange={() => setBtn2(!btn2)} value={btn2}/>
+      </View>
+      <View style={{flexDirection:'row'}}>
+        <Text>Vegan</Text>
+        <Switch trackColor={{true:'red', false:'blue'}} onValueChange={() => setBtn3(!btn3)} value={btn3}/>
+      </View>
+      <View style={{flexDirection:'row'}}>
+        <Text>Vegetarian</Text>
+        <Switch trackColor={{true:'red', false:'blue'}} onValueChange={() => setBtn4(!btn4)} value={btn4}/>
+      </View>
     </View>
   );
 };
+
+FiltersScreen.navigationOptions = (navigationData) => {
+  return {
+    headerLeft: () => {
+      return (
+        <TouchableOpacity
+          onPress={() => {
+            navigationData.navigation.toggleDrawer();
+          }}
+        >
+          <Ionicons name="ios-menu" size={25} />
+        </TouchableOpacity>
+      );
+    },
+    headerRight: () => {
+      return (
+        <TouchableOpacity
+          onPress={() => {
+            navigationData.navigation.toggleDrawer();
+          }}
+        >
+          <Ionicons name="ios-save" size={25} />
+        </TouchableOpacity>
+      );
+    },
+  }
+}
 
 const styles = StyleSheet.create({
   screen: {
--- MealApp2/src/screens/MealDetailScreen.js
+++ MealApp2/src/screens/MealDetailScreen.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import { View, Text, Button, StyleSheet, ScrollView, Image, TouchableOpacity } from "react-native";
 import { MEALS } from "../data/dummy-data";
+import { Ionicons } from "@expo/vector-icons";
 
 const MealDetailScreen = (props) => {
   // เขียนโค้ดเพิ่ม เพื่อดึงอ็อบเจ๊คเมนูอาหารที่ผู้ใช้เลือกเอาไว้
@@ -8,17 +9,30 @@ const MealDetailScreen = (props) => {
   const mealID = props.navigation.getParam("id");
   const meal = MEALS.find((meal) => mealID === meal.id);
   console.log(meal);
+  const renderText = (value, id) =>{
+    return <Text key={id}>{value}</Text>
+  }
+
   return (
     <View style={styles.screen}>
-      <Text>The Meal Detail Screen!</Text>
+
+    <ScrollView>
+      <Image style={styles.image} source={{uri:meal.imageUrl}} />
+    <Text>The Meal Detail Screen!</Text>
+      <Image sr />
       <Text>{meal.title}</Text>
-      <Text>{meal.steps.map((value) => value)}</Text>
+      <Text>Ingredients</Text>
+      {meal.ingredients.map(renderText)}
+      <Text>Steps</Text>
+      {meal.steps.map(renderText)}
       <Button
         title="Go Back to Categories"
         onPress={() => {
           props.navigation.popToTop();
         }}
       />
+
+    </ScrollView>
     </View>
   );
 };
@@ -27,7 +41,14 @@ MealDetailScreen.navigationOptions = (navigationData) => {
   // เขียนโค้ดเพิ่มเพื่อแสดงชื่อเมนูอาหารที่เลือกให้เป็นเฮดเดอร์
   const mealID = navigationData.navigation.getParam("id");
   const meal = MEALS.find((meal) => mealID === meal.id);
-  return { headerTitle: meal.title };
+  return { headerTitle: meal.title, headerRight:() => {
+    return (
+      <TouchableOpacity
+        >
+          <Ionicons name="ios-star" size={25} />
+        </TouchableOpacity>
+    )
+  }};
 };
 
 const styles = StyleSheet.create({
@@ -36,6 +57,10 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
+  image:{
+    width:100,
+    height:100
+  }
 });
 
 export default MealDetailScreen;
